f0f06c64bed320a38283010c513693e7fbad998f
Arya Surya <suryasaputra914@gmail.com>
2025-09-15 13:44:43 +0700

    create tabs layout

---
+++ app/movie/[id].tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const DetailsMovie = () => {
+  return (
+    <View>
+      <Text>DetailsMovie</Text>
+    </View>
+  )
+}
+
+export default DetailsMovie
+
+const styles = StyleSheet.create({})
